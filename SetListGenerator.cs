91cc2ce5b8e16654e1b0c4df2a10886aeb90a16c
PeppersRUs <simon.harte@live.co.uk>
2019-11-13 15:51:35 +0000

    Working on actual list generation. Duplicate checks in the random code required.

--- SetListGenerator.cs
+++ SetListGenerator.cs
@@ -6,10 +6,13 @@ class SetListGenerator
     static void Main(string[] args)
     {
         //How many possible songs to use
-        const int SONG_LIST_SIZE = 4;
+        const int SONG_LIST_SIZE = 10;
 
         //Declare an array of possible songs
         string[] songList = new string[SONG_LIST_SIZE];
+
+        //Random number generator
+        var rnd = new Random();
 
         //Read in all songs
         for (int i = 0; i < SONG_LIST_SIZE; i = i + 1)
@@ -21,16 +24,36 @@ class SetListGenerator
 
         //Print all songs back
         Console.Clear();
-
         Console.WriteLine("Your song list is: ");
         for (int i = 0; i < SONG_LIST_SIZE; i = i + 1)
         {
+            //int j = rnd.Next(songList.Length);
             Console.WriteLine(songList[i]);
         }
 
+        //Declare an empty array for our setlist indices
+        Console.Clear();
+        int setListLength;
+        Console.WriteLine("How long is your setlist?");
+        setListLength = int.Parse(Console.ReadLine());
+        int[] setListNums = new int[setListLength];
 
+        Console.Clear();
 
+        //Randomly generate indices into setListNums
+        for (int i = 0; i < setListLength; i = i + 1)
+        {
+            int j;
+            j = (rnd.Next(0, songList.Length - 1));
+            Console.WriteLine(j);
+            //setListNums.SetValue(i, j);
 
+        }
+
+        //Console.WriteLine(songList.GetValue(j));
+        //string[] setList = new string[setListLength];
+        //Console.WriteLine("Your set list is: ");
+        //Console.WriteLine(songList[j]);
         //["Song 1", "Song 2", "Song 3", "Song 4"];
 
 
